ebca9ce7c36eda260cea4b23704ec714559c0218
bambshot <takeshima.nitkc@gmail.com>
2021-05-03 13:41:35 +0900

    done chapter 3.2

--- src/Picshare.elm
+++ src/Picshare.elm
@@ -2,14 +2,16 @@ module Picshare exposing (main)
 
 import Browser
 import Html exposing (..)
-import Html.Attributes exposing (class, src)
-import Html.Events exposing (onClick)
+import Html.Attributes exposing (class, disabled, placeholder, src, type_, value)
+import Html.Events exposing (onClick, onInput, onSubmit)
 
 
 type alias Model =
     { url : String
     , caption : String
     , liked : Bool
+    , comments : List String
+    , newComment : String
     }
 
 
@@ -25,7 +27,55 @@ initialModel =
     { url = "https://programming-elm.com/" ++ "1.jpg"
     , caption = "Surfing"
     , liked = False
+    , comments = [ "Cowabunga, dude!" ]
+    , newComment = ""
     }
+
+
+viewComment : String -> Html Msg
+viewComment comment =
+    li []
+        [ strong [] [ text "Comment:" ]
+        , text (" " ++ comment)
+        ]
+
+
+viewCommentList : List String -> Html Msg
+viewCommentList comments =
+    case comments of
+        -- コメントないとき
+        [] ->
+            text ""
+
+        -- コメントあるとき
+        _ ->
+            div [ class "comments" ]
+                [ ul []
+                    (List.map viewComment comments)
+                ]
+
+
+viewComments : Model -> Html Msg
+viewComments model =
+    div []
+        [ viewCommentList model.comments
+        , form [ class "new-comment", onSubmit SaveComment ]
+            [ input
+                [ type_ "text"
+                , placeholder "Add a comment..."
+                , value model.newComment
+
+                -- 日本語入力では onInput が正常に動作しないことがあるらしい
+                -- その場合は、`npx elm install elm-community/html-extra` から
+                -- モジュールをインポートして `onChange` に書き換えると良いとか
+                , onInput UpdateComment
+                ]
+                []
+            , button
+                [ disabled (String.isEmpty model.newComment) ]
+                [ text "Save" ]
+            ]
+        ]
 
 
 viewLoveButton : Model -> Html Msg
@@ -55,6 +105,7 @@ viewDetailedPhoto model =
         , div [ class "photo-info" ]
             [ viewLoveButton model
             , h2 [ class "caption" ] [ text model.caption ]
+            , viewComments model
             ]
         ]
 
@@ -71,6 +122,26 @@ view model =
 
 type Msg
     = ToggleLike
+    | UpdateComment String
+    | SaveComment
+
+
+saveNewComment : Model -> Model
+saveNewComment model =
+    let
+        comment =
+            String.trim model.newComment
+    in
+    case comment of
+        "" ->
+            model
+
+        _ ->
+            { model
+              -- Array.prototype.push() 的な. php の要素追加に似てる気もする
+                | comments = model.comments ++ [ comment ]
+                , newComment = ""
+            }
 
 
 update : Msg -> Model -> Model
@@ -78,6 +149,12 @@ update msg model =
     case msg of
         ToggleLike ->
             { model | liked = not model.liked }
+
+        UpdateComment comment ->
+            { model | newComment = comment }
+
+        SaveComment ->
+            saveNewComment model
 
 
 main : Program () Model Msg
